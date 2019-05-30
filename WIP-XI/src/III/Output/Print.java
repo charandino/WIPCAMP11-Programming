b342b9fa689c6139afab40090a127d48a0e7337e
charandino <charonforwork@gmail.com>
2019-05-30 11:37:58 +0700

    [ADD} new project

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,25 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("ใส่ข้อมูลตรงนี้จั๊บ (ไม่ขึ้นบรรทัดใหม่)");
+        
+        System.out.print("อยู่บรรทัดนี้นะ");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("หลังจากนี้ขึ่้นใหม่ \n");
+        
+        System.out.println("อยู่บรรทัดใหม่ละ");
+        System.out.println("แซนด์ รสมัทฉะ");
+        System.out.println("นิว รสซากุระ");
+        
+    }
+}
---
+++ WIP-XI/src/III/Output/_1_Println.java
@@ -0,0 +1,19 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _1_Println {
+    public static void main(String[] args) {
+        String name = "วิป";
+        System.out.println("หวัดดีจั๊บ ชื่อ" + name);
+        
+        System.out.println("ถ้าเราเอา " + 1 + " ไปบวก " + 100 + " เราจะได้ " + 1+100);
+        
+    }
+}
